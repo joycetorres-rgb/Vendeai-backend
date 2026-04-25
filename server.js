fd050cca80e11b1f2199610ce1a3db8cbd6f39f4
joycetorres-rgb <layane.torres@academico.ifpb.edu.br>
2026-04-25 13:07:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const cors = require("cors")
 const axios = require("axios")
 
 const app = express()
+
 app.use(cors())
 app.use(express.json())
+
+app.get("/", (req,res)=>{
+  res.send("Backend VendeAI rodando 🚀")
+})
 
 app.post("/ia", async (req,res)=>{
 
@@ -19,7 +24,7 @@ app.post("/ia", async (req,res)=>{
         messages:[
           {
             role:"system",
-            content:"Você é uma vendedora simpática e persuasiva de loja de roupas. Sempre tente vender produtos."
+            content:"Você é um vendedor especialista"
           },
           {
             role:"user",
@@ -29,7 +34,8 @@ app.post("/ia", async (req,res)=>{
       },
       {
         headers:{
-          Authorization:"Bearer " + process.env.OPENAI_KEY
+          "Authorization": `Bearer ${process.env.OPENAI_KEY}`,
+          "Content-Type":"application/json"
         }
       }
     )
@@ -38,14 +44,16 @@ app.post("/ia", async (req,res)=>{
       reply: resposta.data.choices[0].message.content
     })
 
-  }catch(e){
-    res.json({reply:"Erro na IA"})
+  }catch(err){
+    console.log(err.response?.data || err.message)
+
+    res.json({
+      reply: "Erro na IA 😢"
+    })
   }
 
 })
 
-app.get("/", (req,res)=>{
-  res.send("Backend VendeAI rodando 🚀")
+app.listen(3000, ()=>{
+  console.log("Servidor rodando")
 })
-
-app.listen(3000,()=>console.log("Rodando"))
