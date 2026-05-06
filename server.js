e0226a37aeed9657cc8d9736f77b37d58877f3cb
joycetorres-rgb <layane.torres@academico.ifpb.edu.br>
2026-05-06 18:46:18 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -1,35 +1,51 @@
 
 
+
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 let produtos = [];
 let pedidos = [];
 
+/* TESTE */
 app.get("/", (req,res)=>{
   res.send("🚀 Backend online funcionando");
 });
 
-app.get("/produtos", (req,res)=>{
+/* PRODUTOS */
+
+app.get("/produtos",(req,res)=>{
   res.json(produtos);
 });
 
-app.post("/produtos", (req,res)=>{
+app.post("/produtos",(req,res)=>{
   produtos.push(req.body);
-  res.json({ok:true});
+  res.json({
+    ok:true,
+    produtos
+  });
 });
 
-app.get("/pedidos", (req,res)=>{
+/* PEDIDOS */
+
+app.get("/pedidos",(req,res)=>{
   res.json(pedidos);
 });
 
-app.post("/pedidos", (req,res)=>{
+app.post("/pedidos",(req,res)=>{
   pedidos.push(req.body);
-  res.json({ok:true});
+  res.json({
+    ok:true
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>console.log("rodando"));
+app.listen(PORT, ()=>{
+  console.log("Servidor rodando");
+});
