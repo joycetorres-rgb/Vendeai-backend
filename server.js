2f36abc59af3fabd4b86fe2f6f1d06d5643e73b8
joycetorres-rgb <layane.torres@academico.ifpb.edu.br>
2026-05-06 19:30:06 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -6,43 +6,76 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+/* ================= BANCO TEMPORÁRIO ================= */
+
 let produtos = [];
+
 let pedidos = [];
 
-/* TESTE */
+/* ================= TESTE ================= */
+
 app.get("/", (req,res)=>{
-  res.send("🚀 Backend online funcionando");
+
+res.send("🚀 Backend online funcionando");
+
 });
 
-/* PRODUTOS */
+/* ================= PRODUTOS ================= */
 
 app.get("/produtos",(req,res)=>{
-  res.json(produtos);
+
+res.json(produtos);
+
 });
 
 app.post("/produtos",(req,res)=>{
-  produtos.push(req.body);
-  res.json({
-    ok:true,
-    produtos
-  });
+
+produtos.push(req.body);
+
+res.json({
+ok:true,
+produtos
 });
 
-/* PEDIDOS */
+});
+
+/* ================= EDITAR LISTA ================= */
+
+app.post("/editar-produtos",(req,res)=>{
+
+produtos = req.body;
+
+res.json({
+ok:true,
+produtos
+});
+
+});
+
+/* ================= PEDIDOS ================= */
 
 app.get("/pedidos",(req,res)=>{
-  res.json(pedidos);
+
+res.json(pedidos);
+
 });
 
 app.post("/pedidos",(req,res)=>{
-  pedidos.push(req.body);
-  res.json({
-    ok:true
-  });
+
+pedidos.push(req.body);
+
+res.json({
+ok:true
 });
+
+});
+
+/* ================= SERVIDOR ================= */
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
-  console.log("Servidor rodando");
+app.listen(PORT,()=>{
+
+console.log("Servidor rodando");
+
 });
