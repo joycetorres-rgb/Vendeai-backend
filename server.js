3772396e19aed465130c5d9337fd76a98a537a4d
joycetorres-rgb <layane.torres@academico.ifpb.edu.br>
2026-05-06 17:54:24 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
 
-
-
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("🚀 VendeAI Backend funcionando!");
+app.use(express.json());
+
+let produtos = [];
+let pedidos = [];
+
+/* ================= PRODUTOS ================= */
+
+app.get("/produtos", (req,res)=>{
+  res.json(produtos);
+});
+
+app.post("/produtos", (req,res)=>{
+  produtos.push(req.body);
+  res.json({ok:true});
+});
+
+/* ================= PEDIDOS ================= */
+
+app.get("/pedidos", (req,res)=>{
+  res.json(pedidos);
+});
+
+app.post("/pedidos", (req,res)=>{
+  pedidos.push(req.body);
+  res.json({ok:true});
+});
+
+/* ================= TESTE ================= */
+
+app.get("/", (req,res)=>{
+  res.send("🚀 Backend online funcionando");
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("Servidor rodando na porta " + PORT);
-});
- 
+app.listen(PORT, ()=>console.log("rodando"));
+
